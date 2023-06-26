c8ab4e2e29de4199181e5292e1cb4392db27cd41
Ferli <ferli.120140018@student.itera.ac.id>
2023-06-26 20:27:58 +0700

    login pr2

--- resources/views/Login.blade.php
+++ resources/views/Login.blade.php
@@ -12,65 +12,72 @@
 	<link rel="stylesheet" href="css/login.css">
 
 	</head>
-	<body style="background :none; ">
-	<section class="ftco-section">
-		<div class="container">
-			<div class="row justify-content-center">
-				<div class="col-md-6 text-center mb-5">
-					<h2 class="heading-section">Login</h2>
+	<div class="bodyasoi" style="background-color:rgb(255, 255, 255)">
+		<body style="background-color :white; ">
+			<div class="elemen"><svg width="467" height="240" viewBox="0 0 467 240" fill="none" xmlns="http://www.w3.org/2000/svg">
+				<path d="M482.472 25.5158C481.167 6.64275 465.476 -8 446.558 -8H9.4898C0.770532 -8 -2.88712 3.13262 4.13309 8.30391V8.30391C4.67246 8.70123 5.25435 9.0373 5.86804 9.30593L26.5462 18.3574C28.7954 19.342 31.1394 20.0939 33.5417 20.6013L60.5217 26.3006L114.059 44.1965L145.768 54.7958C148.414 55.6804 150.949 56.8696 153.321 58.3396L435.892 233.478C439.204 235.531 442.828 237.033 446.621 237.924L450.481 238.831C474.08 244.378 496.304 225.486 494.632 201.302L482.472 25.5158Z" fill="url(#paint0_linear_1_51)" fill-opacity="0.9"/>
+				<defs>
+				<linearGradient id="paint0_linear_1_51" x1="-31.7669" y1="-18.9365" x2="461.601" y2="66.1874" gradientUnits="userSpaceOnUse">
+				<stop stop-color="#A6CE39"/>
+				<stop offset="0.473958" stop-color="#A6CE39"/>
+				<stop offset="1" stop-color="#128241"/>
+				</linearGradient>
+				</defs>
+				</svg>
 				</div>
-			</div>
-			<div class="row justify-content-center">
-				<div class="col-md-6 col-lg-4">
-					<div class="login-wrap p-0">
-		      	<form action="#" class="signin-form">
-		      		<div class="form-group">
-		      			<input type="text" class="form-control" placeholder="Username" required style="  border-bottom: 2px solid black;
-                          border-left: none;
-                          border-top: none;
-                          border-right: none;
-                          border-radius:0;" >
-		      		</div>
-	            <div class="form-group">
-	              <input id="password-field" type="password" class="form-control" placeholder="Password" 
-                  required style="border-bottom: 2px solid black;
-                  border-left: none;
-                  border-top: none;
-                  border-right: none;
-                  border-radius:0;">
-	              <span toggle="#password-field" class="fa fa-fw fa-eye field-icon toggle-password"></span>
-	            </div>
-	            <div class="form-group">
-	            	<button type="submit" class="form-control btn btn-primary submit px-3" style="border-color: none; " 
-                    ><svg width="27" height="27"  viewBox="0 0 27 27" fill="none" xmlns="http://www.w3.org/2000/svg" style="margin-right: 5px">
-                        <path d="M27 13.5C27 20.9558 20.9558 27 13.5 27C6.04416 27 0 20.9558 0 13.5C0 6.04416 6.04416 0 13.5 0C20.9558 0 27 6.04416 27 13.5Z" fill="#D9D9D9"/>
-                        <path d="M21.7071 13.7071C22.0976 13.3166 22.0976 12.6834 21.7071 12.2929L15.3431 5.92893C14.9526 5.53841 14.3195 5.53841 13.9289 5.92893C13.5384 6.31946 13.5384 6.95262 13.9289 7.34315L19.5858 13L13.9289 18.6569C13.5384 19.0474 13.5384 19.6805 13.9289 20.0711C14.3195 20.4616 14.9526 20.4616 15.3431 20.0711L21.7071 13.7071ZM7 14L21 14L21 12L7 12L7 14Z" fill="#B3D334"/>
-                        </svg> 
-                        Lanjutkan</button>
-	            </div>
-	            <div class="form-group d-md-flex">
-	            	<div class="w-50">
-		            	<label class="checkbox-wrap checkbox-primary" style="color: rgba(166, 206, 57, 1)">Remember Me
-									  <input type="checkbox" checked>
-									  <span class="checkmark"></span>
-									</label>
-								</div>
-								<div class="w-50 text-md-right">
-									<a href="#" style="color: #fff">Lupa Password Anda?</a>
-								</div>
-	            </div>
-	          </form>
-		      </div>
+			<section class="ftco-section">
+				<div class="container">
+					<div class="row justify-content-center">
+						<div class="col-md-6 text-center mb-5">
+							<h2 class="login-text" style="color: black ">Login</h2>
+						</div>
+					</div>
+					<div class="row justify-content-center">
+						<div class="col-md-6 col-lg-4">
+							<div class="login-wrap p-0">
+						  <form action="#" class="signin-form">
+							  <div class="form-group">
+								  <input type="text" class=" pl-1" placeholder="Nama Pengguna" required style="border-bottom: 2px solid black;">
+							  </div>
+						<div class="form-group">
+						  <input id="password-field" type="password" class=" pl-1" placeholder="Kata Sandi" 
+						  required >
+						  <span toggle="#password-field" class="fa fa-fw fa-eye field-icon toggle-password"></span>
+						</div>
+						<div class="form-group">
+							<button type="submit" class="lanjutkan form-control btn btn-primary submit px-3" style="border-color: none;"" 
+							><svg width="27" height="27"  viewBox="0 0 27 27" fill="none" xmlns="http://www.w3.org/2000/svg" style="margin-right: 5px">
+								<path d="M27 13.5C27 20.9558 20.9558 27 13.5 27C6.04416 27 0 20.9558 0 13.5C0 6.04416 6.04416 0 13.5 0C20.9558 0 27 6.04416 27 13.5Z" fill="#D9D9D9"/>
+								<path d="M21.7071 13.7071C22.0976 13.3166 22.0976 12.6834 21.7071 12.2929L15.3431 5.92893C14.9526 5.53841 14.3195 5.53841 13.9289 5.92893C13.5384 6.31946 13.5384 6.95262 13.9289 7.34315L19.5858 13L13.9289 18.6569C13.5384 19.0474 13.5384 19.6805 13.9289 20.0711C14.3195 20.4616 14.9526 20.4616 15.3431 20.0711L21.7071 13.7071ZM7 14L21 14L21 12L7 12L7 14Z" fill="#B3D334"/>
+								</svg> 
+								Lanjutkan</button>
+						</div>
+						<div class="form-group d-md-flex">
+							<div class="w-50">
+								<label class="checkbox-wrap checkbox-primary" style="color: rgba(166, 206, 57, 1)">Remember Me
+											  <input type="checkbox" checked>
+											  <span class="checkmark"></span>
+											</label>
+										</div>
+										<div class="w-50 text-md-right">
+											<a href="#" style="color: rgba(0, 0, 0, 0.49);
+											font-family: Inter;">Lupa Password Anda?</a>
+										</div>
+						</div>
+					  </form>
+					  </div>
+						</div>
+					</div>
 				</div>
-			</div>
-		</div>
-	</section>
-
-	<script src="js/jquery.min.js"></script>
-  <script src="js/popper.js"></script>
-  <script src="js/bootstrap.min.js"></script>
-  <script src="js/main.js"></script>
-
-	</body>
+			</section>
+		
+			<script src="js/jquery.min.js"></script>
+		  <script src="js/popper.js"></script>
+		  <script src="js/bootstrap.min.js"></script>
+		  <script src="js/main.js"></script>
+		
+			</body>
+	</div>
+	
 </html>
 
