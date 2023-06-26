94c03ea7034634620a718effcb6bcb51b51d9349
Marshallramdh <marshall.120140017@student.itera.ac.id>
2023-06-26 14:49:27 +0700

    navbar

---
+++ resources/views/Dashboard.blade.php
@@ -0,0 +1,5 @@
+@extends('layouts.Navbar')
+
+@section('container')
+    <h1>INI DASHBOARD</h1>
+@endsection
---
+++ resources/views/partials/navbar.blade.php
@@ -0,0 +1,16 @@
+<nav class="navbar navbar-expand-lg navbar-dark" style="background: #A6D17A;">
+    <div class="container">
+        <a class="navbar-brand" href="/"><img src="img/hamburg.png" alt="hamburger" width="40"></a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
+            aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+
+     <div class="corner-text" style="position: absolute; top: 5; right: 0;
+        margin-right: 30px;font-weight: bold;size:48;"> <img src="img/administrator.png" alt="admin" width="40">| Administrator| </div>
+
+        </button>
+            
+        </div>
+    </div>
+  </nav>
