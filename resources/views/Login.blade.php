daba26069239d65e6e24dd74494e1928fb5b23cf
Ferli <ferli.120140018@student.itera.ac.id>
2023-06-27 16:00:33 +0700

    finalv1

--- resources/views/Login.blade.php
+++ resources/views/Login.blade.php
@@ -13,8 +13,40 @@
 
 	</head>
 	<div class="bodyasoi" style="background-color:rgb(255, 255, 255)">
-		<body style="background-color :white; ">
-			<div class="elemen"><svg width="467" height="240" viewBox="0 0 467 240" fill="none" xmlns="http://www.w3.org/2000/svg">
+		<img src="images/logo.png" alt="logo-great-giant-pineapple" id="logo-great-giant-pineapple">
+		<body style="background-color :white; ">	
+			<svg id="circle1"  width="146" height="136" viewBox="0 0 146 136" fill="none" xmlns="http://www.w3.org/2000/svg">
+				<path d="M146 68C146 105.555 113.317 136 73 136C32.6832 136 0 105.555 0 68C0 30.4446 32.6832 0 73 0C113.317 0 146 30.4446 146 68Z" fill="#D7DF23" fill-opacity="0.5"/>
+				</svg>
+				
+			<svg id="circle2"  width="100" height="100" viewBox="0 0 100 100" fill="none" xmlns="http://www.w3.org/2000/svg">
+					<path d="M100 50C100 77.6142 77.6142 100 50 100C22.3858 100 0 77.6142 0 50C0 22.3858 22.3858 0 50 0C77.6142 0 100 22.3858 100 50Z" fill="#128241" fill-opacity="0.53"/>
+				</svg>
+
+			<svg  id="circle3" width="55" height="55" viewBox="0 0 55 55" fill="none" xmlns="http://www.w3.org/2000/svg">
+					<path d="M55 27.5C55 42.6878 42.6878 55 27.5 55C12.3122 55 0 42.6878 0 27.5C0 12.3122 12.3122 0 27.5 0C42.6878 0 55 12.3122 55 27.5Z" fill="#D7DF23"/>
+					</svg>
+
+								
+				<div class="arrowcircle">
+						<svg id="circlecoak" width="25" height="27" viewBox="0 0 25 27" fill="none" xmlns="http://www.w3.org/2000/svg">
+							<path d="M20.2071 14.6095C20.5976 14.2189 20.5976 13.5858 20.2071 13.1952L13.8431 6.83128C13.4526 6.44076 12.8194 6.44076 12.4289 6.83129C12.0384 7.22181 12.0384 7.85498 12.4289 8.2455L18.0858 13.9023L12.4289 19.5592C12.0384 19.9497 12.0384 20.5829 12.4289 20.9734C12.8195 21.3639 13.4526 21.3639 13.8432 20.9734L20.2071 14.6095ZM0.72754 14.9024L19.5 14.9023L19.5 12.9023L0.727538 12.9024L0.72754 14.9024Z" fill="#B3D334"/>
+							<mask id="path-2-inside-1_1_57" fill="white">
+							<path d="M0.7821 8.79973C1.82112 5.77839 3.82671 3.25821 6.43633 1.69476C9.04594 0.131311 12.0874 -0.37224 15.0108 0.275138C17.9342 0.922515 20.5467 2.6781 22.3761 5.22453C24.2054 7.77097 25.1308 10.9402 24.9851 14.1593C24.8393 17.3785 23.632 20.4351 21.5814 22.7766C19.5307 25.1182 16.7721 26.5901 13.8041 26.9263C10.8361 27.2626 7.85472 26.441 5.39879 24.61C2.94287 22.779 1.17452 20.0595 0.413392 16.9431L2.24469 16.4215C2.89049 19.0657 4.39092 21.3731 6.47473 22.9267C8.55854 24.4802 11.0882 25.1773 13.6065 24.892C16.1248 24.6067 18.4655 23.3578 20.2054 21.3711C21.9453 19.3843 22.9697 16.7908 23.0934 14.0594C23.2171 11.3281 22.4319 8.639 20.8797 6.47839C19.3275 4.31778 17.1108 2.82819 14.6304 2.2789C12.1499 1.72961 9.56928 2.15686 7.35506 3.48343C5.14084 4.80999 3.43913 6.94833 2.55753 9.51189L0.7821 8.79973Z"/>
+							</mask>
+							<path d="M0.7821 8.79973C1.82112 5.77839 3.82671 3.25821 6.43633 1.69476C9.04594 0.131311 12.0874 -0.37224 15.0108 0.275138C17.9342 0.922515 20.5467 2.6781 22.3761 5.22453C24.2054 7.77097 25.1308 10.9402 24.9851 14.1593C24.8393 17.3785 23.632 20.4351 21.5814 22.7766C19.5307 25.1182 16.7721 26.5901 13.8041 26.9263C10.8361 27.2626 7.85472 26.441 5.39879 24.61C2.94287 22.779 1.17452 20.0595 0.413392 16.9431L2.24469 16.4215C2.89049 19.0657 4.39092 21.3731 6.47473 22.9267C8.55854 24.4802 11.0882 25.1773 13.6065 24.892C16.1248 24.6067 18.4655 23.3578 20.2054 21.3711C21.9453 19.3843 22.9697 16.7908 23.0934 14.0594C23.2171 11.3281 22.4319 8.639 20.8797 6.47839C19.3275 4.31778 17.1108 2.82819 14.6304 2.2789C12.1499 1.72961 9.56928 2.15686 7.35506 3.48343C5.14084 4.80999 3.43913 6.94833 2.55753 9.51189L0.7821 8.79973Z" stroke="#B3D334" stroke-width="6" mask="url(#path-2-inside-1_1_57)"/>
+						</svg>			
+				</div>
+
+				<img src="images/footer-wave.png" alt="footer-wave" id="footer-wave" >
+							
+				<div class="button-container">
+					<button>Kontak</button>
+					<button>Blog</button>
+					<button id="btn-login">Login</button>
+				  </div>
+				  
+			<div class="elemen mt-0"><svg width="467" height="240" viewBox="0 0 467 240" fill="none" xmlns="http://www.w3.org/2000/svg">
 				<path d="M482.472 25.5158C481.167 6.64275 465.476 -8 446.558 -8H9.4898C0.770532 -8 -2.88712 3.13262 4.13309 8.30391V8.30391C4.67246 8.70123 5.25435 9.0373 5.86804 9.30593L26.5462 18.3574C28.7954 19.342 31.1394 20.0939 33.5417 20.6013L60.5217 26.3006L114.059 44.1965L145.768 54.7958C148.414 55.6804 150.949 56.8696 153.321 58.3396L435.892 233.478C439.204 235.531 442.828 237.033 446.621 237.924L450.481 238.831C474.08 244.378 496.304 225.486 494.632 201.302L482.472 25.5158Z" fill="url(#paint0_linear_1_51)" fill-opacity="0.9"/>
 				<defs>
 				<linearGradient id="paint0_linear_1_51" x1="-31.7669" y1="-18.9365" x2="461.601" y2="66.1874" gradientUnits="userSpaceOnUse">
@@ -24,7 +56,7 @@
 				</linearGradient>
 				</defs>
 				</svg>
-				</div>
+			</div>
 			<section class="ftco-section">
 				<div class="container">
 					<div class="row justify-content-center">
@@ -37,7 +69,7 @@
 							<div class="login-wrap p-0">
 						  <form action="#" class="signin-form">
 							  <div class="form-group">
-								  <input type="text" class=" pl-1" placeholder="Nama Pengguna" required style="border-bottom: 2px solid black;">
+								  <input type="text" class=" pl-1 mb-3"  placeholder="Nama Pengguna" required style="border-bottom: 2px solid black;">
 							  </div>
 									<div class="form-group">
 										<input id="password-field" type="password" class=" pl-1" placeholder="Kata Sandi" 
