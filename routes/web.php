1ed018c77994046eafc4141013a5cf7c332f01aa
rYuuXHikaRi <ryuux.devcode@gmail.com>
2023-06-28 08:41:44 +0700

    Update web.php
    Adding comment to change route between Login and Dashboard page

--- routes/web.php
+++ routes/web.php
@@ -17,7 +17,7 @@ use Illuminate\Support\Facades\Route;
 
 
 Route::get('/', function () {
-    return view('Login');
+    return view('Dashboard'); // Change to login again
 });
 
 Route::get('/Dashboard', function () {
