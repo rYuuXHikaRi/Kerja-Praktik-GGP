6d24ba885ebd581ccd0f4e938b24fee767befe17
rYuuXHikaRi <ryuux.devcode@gmail.com>
2023-06-30 09:14:09 +0700

    Update web.php
    Add Login Route

--- routes/web.php
+++ routes/web.php
@@ -17,7 +17,7 @@ use Illuminate\Support\Facades\Route;
 
 
 Route::get('/', function () {
-    return view('Dashboard'); // Change to login again
+    return view('Login'); // Change to login again
 });
 
 Route::get('/Dashboard', function () {
