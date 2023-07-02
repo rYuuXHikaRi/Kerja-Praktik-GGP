2dbdaec4a8331f3b4f15d9a5d5f6040683466db9
rYuuXHikaRi <ryuux.devcode@gmail.com>
2023-07-02 07:13:29 +0700

    Update KelolaArsip.blade.php
    Fix unclosed <div> tag

--- resources/views/KelolaArsip.blade.php
+++ resources/views/KelolaArsip.blade.php
@@ -7,27 +7,27 @@
         <div class="card mt-5" style="padding: 10px;border-radius: 10px">
             <div class="panel-body">
                 <div class="control-group after-add-more">
-                    <h5>Tambah Dokumen</h5>
-                    <hr>
-                    <label for="namadokumen" class="nama-dokumen">Nama Dokumen</label>
-                    <input id="namadokumen"  name='NamaDokumen' type="text"><br>
-                    <br>
-                    <label for="keterangan" class="ket">Keterangan</label>
-                    <input id="keterangan" name='Keterangan' type="text">
-                    <br>
-                    <br>
-                    <label for="namadesa" class="nama-desa">Nama Desa</label>
-                    <input id="namadesa" name='NamaDesa' type="text">
-                    <br>
-                    <br>
-                    <label for="tahun" class="thn">Tahun</label>
-                    <input id="tahun" name='Tahun'  type="text">
-                    <br>
-                    <br>
-                    <label for="lokasipenyimpanan" class="lok">Lokasi Penyimpanan</label>
-                    <input id="lokasipenyimpanan" name='LokasiPenyimpanan' type="text">
-                    <br>
-                    <br>
+                  <h5>Tambah Dokumen</h5>
+                  <hr>
+                  <label for="namadokumen" class="nama-dokumen">Nama Dokumen</label>
+                  <input id="namadokumen"  name='NamaDokumen' type="text"><br>
+                  <br>
+                  <label for="keterangan" class="ket">Keterangan</label>
+                  <input id="keterangan" name='Keterangan' type="text">
+                  <br>
+                  <br>
+                  <label for="namadesa" class="nama-desa">Nama Desa</label>
+                  <input id="namadesa" name='NamaDesa' type="text">
+                  <br>
+                  <br>
+                  <label for="tahun" class="thn">Tahun</label>
+                  <input id="tahun" name='Tahun'  type="text">
+                  <br>
+                  <br>
+                  <label for="lokasipenyimpanan" class="lok">Lokasi Penyimpanan</label>
+                  <input id="lokasipenyimpanan" name='LokasiPenyimpanan' type="text">
+                  <br>
+                  <br>
                   <label>Upload File</label>
                   <input type='file' name='NamaFile' accept='image/*'>
                   <br>
