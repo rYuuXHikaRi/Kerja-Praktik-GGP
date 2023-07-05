5d87aa73a0dfd4e86e931ee52aa2652c746f4e30
Marshallramdh <marshall.120140017@student.itera.ac.id>
2023-07-05 14:46:34 +0700

    Update Profil.blade.php
    memasukkan ganti foto ke tag form

--- resources/views/Profil.blade.php
+++ resources/views/Profil.blade.php
@@ -9,36 +9,39 @@
       <!-- /.card-header -->
       <div class="card-body" style="height: 350px;">
         <div class="profile">
-          <div class="profile-image" style="display: flex; flex-direction:column;">
-            <img src="img/profil.jpeg" alt="Foto Profil" style="width: 15vh">
-            <div class="mt-2">
-              <label for="foto-input" class="btn submit-btn-yes" style="width:15vh;">Ganti Foto</label>
-              <input type="file" id="foto-input" name="foto" accept="image/*" >
-            </div>
-          </div>
-          <div class="profile-form">
-            <form>
-
-                <label for="name">Nama:</label>
-                <input class="mb-3"type="text" id="name" name="name">
-
-                <label for="username">Username:</label>
-                <input class="mb-3"type="username" id="username" name="username">
-
-                <label for="phone">Nomor Telepon:</label>
-                <input class="mb-3"type="text" id="phone" name="phone">
-
-                <label for="PasswordLama">Password Lama:</label>
-                <input class="mb-3"type="PasswordLama" id="PasswordLama" name="PasswordLama">
-
-                <label for="PasswordBaru">Password Baru:</label>
-                <input class="mb-3"type="PasswordBaru" id="PasswordBaru" name="PasswordBaru">
-                <br><br><br><br><br><br><br><br><br><br><br>
-                <div style="display: flex; justify-content:flex-end;">
-                  <button type="submit" class="btn submit-btn-yes save-btn">Simpan</button>
+          <form>
+            <div style="display: flex;">
+              <div class="profile-image" style="display: flex; flex-direction: column;">
+                <img src="img/profil.jpeg" alt="Foto Profil" style="width: 15vh">
+                <div class="mt-2">
+                  <label for="foto-input" class="btn submit-btn-yes" style="width:15vh;">Ganti Foto</label>
+                  <input type="file" id="foto-input" name="foto" accept="image/*">
                 </div>
-            </form>
-          </div>
+              </div>
+              <div style="margin-left: 20px;">
+                <label for="name">Nama:</label>
+                <input class="mb-3" type="text" id="name" name="name">
+          
+                <label for="username">Username:</label>
+                <input class="mb-3" type="username" id="username" name="username">
+          
+                <label for="phone">Nomor Telepon:</label>
+                <input class="mb-3" type="text" id="phone" name="phone">
+          
+                <label for="PasswordLama">Password Lama:</label>
+                <input class="mb-3" type="PasswordLama" id="PasswordLama" name="PasswordLama">
+          
+                <label for="PasswordBaru">Password Baru:</label>
+                <input class="mb-3" type="PasswordBaru" id="PasswordBaru" name="PasswordBaru">
+              </div>
+            </div>
+            
+            <div style="display: flex; justify-content:flex-end;">
+              <button type="submit" class="btn submit-btn-yes save-btn">Simpan</button>
+            </div>
+          </form>
+          
+          
         </div>
       </div>
       <!-- /.card-body -->
