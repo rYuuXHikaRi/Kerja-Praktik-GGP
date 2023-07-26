efb78269d308623fcd088afd6087ca5fe26fb8c4
Ferli <ferli.120140018@student.itera.ac.id>
2023-07-26 09:57:25 +0700

    delete api user

--- app/Http/Controllers/Api/UserApiController.php
+++ app/Http/Controllers/Api/UserApiController.php
@@ -18,58 +18,31 @@ class UserApiController extends Controller
 
     public function store(Request $request)
     {
-        //     $validatedData = $request->validate([
-
-        //         'Foto' => 'required|mimes:jpeg,png,jpg,gif|max:5120 ',
-        //         'UserName' => [
-        //             'required',
-        //             Rule::unique('users')->where(function ($query) use ($request) {
-        //                 return $query->where('UserName', $request->UserName);
-        //             }),
-        //         ],
-        //     ]);
-
-        //     $file = $validatedData[('Foto')];
-        //     $filename =  $file->getClientOriginalName();
-        //     // File upload location
-        //     $location = '../public/assets/images/';
-        //     // Validasi request jika diperlukan
-        //     $validatedData = $request->validate([
-        //         'NamaLengkap' => 'required',
-        //         'UserName' => 'required|unique:users',
-        //         'NomorHp' => 'required',
-        //         'Foto' => 'required',
-        //         'Roles' => 'required',
-        //         'password' => 'required|min:6',
-        //         'id' => 'required',
-        //     ]);
-
-        //     // Buat data user baru
-        //     $user = User::create([
-        //         'NamaLengkap' => $validatedData['NamaLengkap'],
-        //         'UserName' => $validatedData['UserName'],
-        //         'NomorHp' => $validatedData['NomorHp'],
-        //         'Foto' => $validatedData['Foto'],
-        //         'Roles' => $validatedData['Roles'],
-        //         'password' => bcrypt($validatedData['password']),
-        //         // 'id' => $validatedData['id'],
-        //     ]);
-
-        //     // Kirim respons
-        //     return response()->json([
-        //         'message' => 'User created successfully',
-        //         'user' => $user,
-        //     ], 201);
+        // Validasi permintaan untuk memastikan file gambar diunggah
+        $request->validate([
+            'Foto' => 'required|image|mimes:jpeg,png,jpg,gif|max:2048',
+        ]);
+    
+        // Mengambil file gambar dari permintaan
+        $file = $request->file('Foto');
+    
+        // Menyimpan file gambar ke direktori yang sesuai (misalnya, direktori 'public/images')
+        $fileName = time() . '.' . $file->getClientOriginalExtension();
+        $file->storeAs('public/images', $fileName);
+    
+        // Menyimpan data pengguna ke dalam database dengan nama file gambar yang disimpan
         User::create([
             'NamaLengkap' => $request->NamaLengkap,
             'UserName' => $request->UserName,
-            'password' =>  Hash::make($request->Password),
+            'password' => Hash::make($request->Password),
             'NomorHp' => $request->NomorHp,
-            'Foto' => "foto",
-            'Roles' => "1"
+            'Foto' => $fileName, // Menggunakan nama file yang disimpan dalam kolom 'Foto'
+            'Roles' => $request->Roles,
         ]);
+    
         return response()->json(201);
     }
+    
 
     public function show(User $user)
     {
