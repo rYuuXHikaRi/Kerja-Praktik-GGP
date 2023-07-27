40ce3251f0ab53105384f05ba87b998337f90d30
Ferli <ferli.120140018@student.itera.ac.id>
2023-07-27 14:14:29 +0700

    buat endpoint destroy arsip

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -78,8 +78,6 @@ class UserController extends Controller
         return view('admin.user.edit', compact('user'));
     }
 
-
-
     public function update(Request $request, $id)
     {
         $user = User::where('id', $id)->first();
